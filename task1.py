0921c7e6b9e778621d48a22321ed6924f94e09c9
Motochka <motochka111@gmail.com>
2024-12-17 11:02:22 -0500

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+print(12 + 89 - 1)
+#1st program
+print((9 ** 0.5) * 5)
+#2nd prog
+print(9.99 >9.98 , 1000!=1000.1)
+#3rd prog
+print((2*2+2) == (2*(2+2)))
+#4th prog
+print(int(float(123.456*10)%10))
